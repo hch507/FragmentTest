c295a29558c8e6d9371e6693e320e654965bb430
chungHee Han <chunghuihan9gmail.com>
2022-02-15 11:14:49 +0900

    second commit

--- app/src/main/java/com/example/fragmenttest/fragment/secondfrag.kt
+++ app/src/main/java/com/example/fragmenttest/fragment/secondfrag.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.fragmenttest.R
+import kotlinx.android.synthetic.main.fragment_fristfrag.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +24,8 @@ class secondfrag : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    lateinit var navController: NavController
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +41,15 @@ class secondfrag : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_secondfrag, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController= Navigation.findNavController(view)
+
+        btn_next.setOnClickListener{
+            navController.navigate(R.id.action_secondfrag_to_thirdfrag)
+        }
     }
 
     companion object {
